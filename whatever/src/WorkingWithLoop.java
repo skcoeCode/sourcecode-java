f23221115b18ea490545cb86765f85f627f22a9c
skcoeCode <skcoe12345678@gmail.com>
2018-07-26 11:44:14 +0700

    WorkShop Sum from 1 to 10

--- whatever/src/WorkingWithLoop.java
+++ whatever/src/WorkingWithLoop.java
@@ -2,11 +2,15 @@
 public class WorkingWithLoop {
 
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            
-        }
-        
+        sum10();
+    }
 
+    private static void sum10() {
+        int total=0;
+        for (int i = 1; i < 10; i++) {
+            total=total+i;
+        }
+        System.out.println(total);
     }
 
 }
